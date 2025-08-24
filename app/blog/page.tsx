f73de86b4400ceaf1930fe5b24fd79eb1ae6e2a0
sandheep rajkumar <rajkumar.sandheep@gmail.com>
2025-08-24 19:24:12 +0530

    cleaner separators

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -68,7 +68,7 @@ export default function BlogPage() {
             <React.Fragment key={year}>
               {idx > 0 && (
                 <div className="my-4 relative z-[160]">
-                  <Separator className="bg-foreground/60" />
+                  <Separator className="bg-border/60" />
                 </div>
               )}
               <YearSection 
